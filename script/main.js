2021c25b3c2f1c6d267cab9835396f2d7e3cfce4
dev-sharafat <engsharafat23@gmail.com>
2025-03-02 23:04:06 +0600

    Now the website is ready to go

--- script/main.js
+++ script/main.js
@@ -17,7 +17,7 @@ for (let i = 0; i < buttons.length; i++) {
             alert("Board Updated Successfully!")
         }
         if (taskAssigned <= 1) {
-            alert("Congrats!! you have successfully completed all the current task");
+            alert("Congrates!! you have successfully completed all the current task");
         }
         document.getElementById("task-assigned").innerText = convertTaskAssigned - 1;
         document.getElementById("mark-number").innerText = convertMarkOfNumber + 1;
